d3af55be2a8fba188aae93dc656056382018f957
saeko yano <saekoy16@gmail.com>
2020-12-11 04:24:48 +0000

    sampleを再送信

--- intro-ruby/test.rb
+++
@@ -1,7 +0,0 @@
-car = "フェラーリ"
-puts "私の好きな車は"+car
-puts "高級車といったら"+car
-puts "私の好きな車は#{car}"
-puts "高級車といったら#{car}"
-age = 30
-puts "#{age}歳になります"
--- jQuery-ex2/main.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('#accordion dd').hide();
-  $('#accordion dt').on('click', function(){
-    $(this).next('dd').slideToggle('slow');
-  });
-});
